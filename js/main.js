53f425f5880de3dd3821190889927385382b50d9
Ram-Gopal132 <ramgopalgupta053@gmail.com>
2026-01-09 17:03:24 +0530

    Write some code

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ const showMenu = (navToggle, navMenu) =>{
     })
 }
 showMenu('navToggle','navMenu')
+
+
+// active and remove menu
+
+const navLink = document.querySelectorAll('.nav_link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('navMenu')
+    // when we click on each nav__link, we remove the show_menu class
+    navMenu.classList.remove('show_menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
